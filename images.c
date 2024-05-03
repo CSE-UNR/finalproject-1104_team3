3d2b60c4d70cf3877892df241fbec3333b3019b0
DJ DeLucca <ddelucca@nevada.unr.edu>
2024-05-03 21:18:01 +0000

    project file

--- images.c
+++ images.c
@@ -5,6 +5,7 @@
 */
 
 #include <stdio.h>
+#define LENGTH 100
 
 void loadImage();
 void displayImage();
@@ -17,6 +18,7 @@ void saveImage();
 
 int main(){
 	int choice;
+	FILE *fptr; 
 	
 	do{
 		//print main menu
@@ -32,13 +34,14 @@ int main(){
 		if(choice == 1){
 			printf("\nLoad was selected.\n\n");
 		}
+		
 		//if the user enters 2, display the image
 		else if(choice == 2){
 			printf("\nDisplay was selected.\n\n");
 		}
 		//if the user enters 3, edit the image
 		else if(choice == 3){
-			printf("\nEdit was selected.\n\n");
+			editImage();
 		}
 		//invalid case
 		else if(choice != 1 && choice != 2 && choice != 3 && choice != 0){
@@ -55,7 +58,7 @@ int main(){
 }
 //DJ
 void loadImage(){
-
+	
 }
 
 //DJ and Rajat
@@ -65,7 +68,45 @@ void displayImage(){
 
 //DJ
 void editImage(){
-
+	int option;
+	do{
+		//print main menu
+		printf("**EDITING**\n");
+		printf("1: Crop image\n");
+		printf("2: Dim image\n");
+		printf("3: Brighten image\n");
+		printf("4: Rotate image\n");
+		printf("0: Exit\n");
+		printf("\nChoose from one of the options above: ");
+		scanf("%d", &option);
+		
+		//if the user enters 1, crop the image
+		if(option == 1){
+			printf("\nCrop was selected.\n\n");
+		}
+		
+		//if the user enters 2, dim the image
+		else if(option == 2){
+			printf("\nDim was selected.\n\n");
+		}
+		//if the user enters 3, brighten the image
+		else if(option == 3){
+			printf("\nBrighten was selected.\n\n");
+		}
+		//if the user enters 4, rotate the image
+		else if(option == 4){
+			printf("\nRotate was selected.\n\n");
+		}
+		//invalid case
+		else if(option != 1 && option != 2 && option != 3 && option != 4 && option != 0){
+			printf("\nInvalid option, please try again.\n\n");
+		}
+		
+	}while(option != 0);
+	//if the user enters 0, the program returns to the main menu
+	if(option == 0){
+		printf("\nReturning to main menu\n\n");
+	}
 }
 
 //Rajat
