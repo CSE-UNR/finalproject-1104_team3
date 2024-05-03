c22a9d4e52f586d57f196419cc03f14a6345050d
DJ DeLucca <ddelucca@nevada.unr.edu>
2024-05-03 19:42:44 +0000

    project file

--- images.c
+++ images.c
@@ -16,6 +16,40 @@ void rotateImage();
 void saveImage();
 
 int main(){
+	int choice;
+	
+	do{
+		//print main menu
+		printf("**ERINSTAGRAM**\n");
+		printf("1: Load image\n");
+		printf("2: Display image\n");
+		printf("3: Edit image\n");
+		printf("0: Exit\n");
+		printf("\nChoose from one of the options above: ");
+		scanf("%d", &choice);
+		
+		//if the user enters 1, load the image
+		if(choice == 1){
+			printf("\nLoad was selected.\n\n");
+		}
+		//if the user enters 2, display the image
+		else if(choice == 2){
+			printf("\nDisplay was selected.\n\n");
+		}
+		//if the user enters 3, edit the image
+		else if(choice == 3){
+			printf("\nEdit was selected.\n\n");
+		}
+		//invalid case
+		else if(choice != 1 && choice != 2 && choice != 3 && choice != 0){
+			printf("\nInvalid option, please try again.\n\n");
+		}
+		
+	}while(choice != 0);
+	//if the user enters 0, the program ends
+	if(choice == 0){
+		printf("\nGoodbye!\n\n");
+	}
 	
 	return 0;
 }
