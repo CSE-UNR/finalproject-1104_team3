f8cf043f78e6827708372a7b0f1ac7829b8840a0
DJ DeLucca <ddelucca@nevada.unr.edu>
2024-05-03 19:05:07 +0000

    project file

---
+++ images.c
@@ -0,0 +1,60 @@
+/**
+*Authors: DJ DeLucca and Rajat Sharma
+*Date: 5/3/2024
+*Purpose: Make an image processing program
+*/
+
+#include <stdio.h>
+
+void loadImage();
+void displayImage();
+void editImage();
+void cropImage();
+void dimImage();
+void brightenImage();
+void rotateImage();
+void saveImage();
+
+int main(){
+	
+	return 0;
+}
+//DJ
+void loadImage(){
+
+}
+
+//DJ and Rajat
+void displayImage(){
+
+}
+
+//DJ
+void editImage(){
+
+}
+
+//Rajat
+void cropImage(){
+
+}
+
+//Rajat
+void dimImage(){
+
+}
+
+//Rajat
+void brightenImage(){
+
+}
+
+//DJ and Rajat
+void rotateImage(){
+
+}
+
+//DJ
+void saveImage(){
+
+}
